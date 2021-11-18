860db5e0a677545b5191fb3445125a5b7b4446ce
王广涛 <guangtao.wang@jd.com>
2021-11-18 09:41:53 -0800

    Initial commit

--- run_proppred_pretrain.py
+++ run_proppred_pretrain.py
@@ -7,6 +7,8 @@ from tensorboardX import SummaryWriter
 from codes.argument_parser import default_parser, json_to_argv, complete_default_parser
 from codes.citation_graph_data import citation_subgraph_pretrain_dataloader
 from codes.citation_graph_data import citation_node_pred_subgraph_data_helper
+from codes.ogb_graph_data import ogb_subgraph_pretrain_dataloader
+from codes.ogb_graph_data import ogb_node_pred_subgraph_data_helper
 from codes.gnn_encoder import GraphSimSiamEncoder
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
@@ -33,14 +35,21 @@ for key, value in vars(args).items():
     logger.info('Hype-parameter\t{} = {}'.format(key, value))
 logging.info('*' * 75)
 #########################################################################
-citation_pretrain_dataloader, node_features, n_classes = citation_subgraph_pretrain_dataloader(args=args)
-logging.info('Loading pretrained data = {} completed'.format(len(citation_pretrain_dataloader)))
-logging.info('*' * 75)
-citation_data_helper = citation_node_pred_subgraph_data_helper(args=args)
-citation_train_dataloader = citation_data_helper.data_loader(data_type='train')
-logging.info('Loading training data = {} completed'.format(len(citation_train_dataloader)))
-citation_val_dataloader = citation_data_helper.data_loader(data_type='valid')
-logging.info('Loading validation data = {} completed'.format(len(citation_val_dataloader)))
+if args.graph_type == 'citation':
+    pretrain_dataloader, node_features, n_classes = citation_subgraph_pretrain_dataloader(args=args)
+    logging.info('Loading pretrained data = {} for {} completed'.format(len(pretrain_dataloader), args.graph_type))
+    logging.info('*' * 75)
+    node_data_helper = citation_node_pred_subgraph_data_helper(args=args)
+else:
+    pretrain_dataloader, node_features, n_classes = ogb_subgraph_pretrain_dataloader(args=args)
+    logging.info('Loading pretrained data = {} for {} completed'.format(len(pretrain_dataloader), args.graph_type))
+    logging.info('*' * 75)
+    node_data_helper = ogb_node_pred_subgraph_data_helper(args=args)
+
+train_dataloader = node_data_helper.data_loader(data_type='train')
+logging.info('Loading training data = {} completed'.format(len(train_dataloader)))
+val_dataloader = node_data_helper.data_loader(data_type='valid')
+logging.info('Loading validation data = {} completed'.format(len(val_dataloader)))
 logging.info('*' * 75)
 #########################################################################
 for key, value in vars(args).items():
@@ -55,13 +64,13 @@ graph_encoder.to(args.device)
 # # Print model information
 # #########################################################################
 if args.total_pretrain_steps > 0:
-    t_total_steps = args.total_pretrain_steps
-    args.num_pretrain_epochs = args.total_pretrain_steps // (len(citation_pretrain_dataloader)
+    t_total_pretrain_steps = args.total_pretrain_steps
+    args.num_pretrain_epochs = args.total_pretrain_steps // (len(pretrain_dataloader)
                                                              // args.gradient_accumulation_steps) + 1
 else:
-    t_total_steps = len(citation_pretrain_dataloader) // args.gradient_accumulation_steps \
+    t_total_pretrain_steps = len(pretrain_dataloader) // args.gradient_accumulation_steps \
                     * args.num_pretrain_epochs
-optimizer, scheduler = graph_encoder.pretrain_optimizer_scheduler(total_steps=t_total_steps)
+optimizer, scheduler = graph_encoder.pretrain_optimizer_scheduler(total_steps=t_total_pretrain_steps)
 # #########################################################################
 global_step = 0
 if args.local_rank in [-1, 0]:
@@ -84,7 +93,7 @@ logging.info('Staring Pretraining over graph = {}'.format(args.citation_name))
 pretrain_iterator = trange(start_epoch, start_epoch+int(args.num_pretrain_epochs), desc="Epoch",
                            disable=args.local_rank not in [-1, 0])
 for epoch_idx, epoch in enumerate(pretrain_iterator):
-    epoch_iterator = tqdm(citation_pretrain_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
+    epoch_iterator = tqdm(pretrain_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
     for step, batch in enumerate(epoch_iterator):
         graph_encoder.train()
         # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
